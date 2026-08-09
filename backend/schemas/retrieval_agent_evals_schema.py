edb95d29b9f116e2ec3f1beb4f481c8057b32f8f
Ali Seena <aliwork0066@gmail.com>
2026-08-09 18:32:55 +0500

    feat: modified the schema for agent evals

--- backend/schemas/retrieval_agent_evals_schema.py
+++ backend/schemas/retrieval_agent_evals_schema.py
@@ -9,4 +9,5 @@ class RetrievalAgentEvalsSchema(BaseModel):
 
     reasoning: str = Field(description="The reasoning behind the evaluation of the search results.")
     sufficient: Literal["True", "False"] = Field(description="Indicates whether the search results were sufficient to answer the question.")
+    focused: Literal["True", "False"] = Field(description="indicates whether the search results were focused.")
     confidence: float = Field(description="A confidence score (0-1) indicating the evaluator's confidence in their assessment of the search results.", ge=0, le=1)

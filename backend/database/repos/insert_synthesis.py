93c9c654291eaf88df4436af091da582df1d59a2
Ali Seena <aliwork0066@gmail.com>
2026-08-20 22:24:31 +0500

    refactor: updated insert function

--- backend/database/repos/insert_synthesis.py
+++ backend/database/repos/insert_synthesis.py
@@ -7,7 +7,8 @@ from database.session import SessionLocal
 def insert_synthesis(
         execution_id: UUID,
         answer: str = "No Answer",
-        reasoning_summary: str = "No Reasoning"
+        reasoning_summary: str = "No Reasoning",
+        citations: list[dict] = None
     ):
     db = SessionLocal()
 
@@ -15,7 +16,8 @@ def insert_synthesis(
         record = SynthesisTable(
             execution_id=execution_id,
             answer=answer,
-            reasoning_summary=reasoning_summary
+            reasoning_summary=reasoning_summary,
+            citations=citations
         )
 
         db.add(record)
